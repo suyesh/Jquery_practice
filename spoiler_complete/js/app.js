d1b57f2fbb5f8f649fff59ed4d4e49c9732af68e
Suyesh Bhandari <suyeshb@gmail.com>
2016-03-02 13:23:45 -0500

    Buttons to reveal working

--- spoiler_complete/js/app.js
+++ spoiler_complete/js/app.js
@@ -8,7 +8,7 @@ $(".spoiler").append("<button>Reveal Spoiler!</button>");
 //3- When button is pressed
 $("button").click(function(){
     //3.1 Show the spoiler.
-    $(".spoiler span").show();
+    $(this).prev().show();
     //3.2 Get rid of button
     $(this).remove();
 });
