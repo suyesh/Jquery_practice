8ed4fbdaa516ecff0b380ae0d7869cfcfe884e99
Suyesh Bhandari <suyeshb@gmail.com>
2016-03-02 12:46:34 -0500

    Added Button and Hid the span with spoilers

--- spoiler_complete/js/app.js
+++ spoiler_complete/js/app.js
@@ -2,7 +2,9 @@
 //solutions: Hide spoilers and reveal them through user interactions
 
 //1- Hide spoler
+$(".spoiler span").hide();
 //2- Add a button
+$(".spoiler").append("<button>Reveal Spoiler!</button>");
 //3- When button is pressed
     //3.1 Show the spoiler.
     //3.2 Get rid of button
