b0eebcf7640af2353d7721636207626542d2c90f
Suyesh Bhandari <suyeshb@gmail.com>
2016-03-02 12:22:28 -0500

    Wrote Plan

--- spoiler_complete/js/app.js
+++ spoiler_complete/js/app.js
@@ -1,2 +1,8 @@
-//Prevent spoilerphobes from seeing spoilerphobes
+//Prevent spoilerphobes from seeing spoilers
 //solutions: Hide spoilers and reveal them through user interactions
+
+//1- Hide spoler
+//2- Add a button
+//3- When button is pressed
+    //3.1 Show the spoiler.
+    //3.2 Get rid of button
